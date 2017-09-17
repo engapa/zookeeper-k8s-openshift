a75fa5cee4094282e18a51b4fa742665d8f09786
Enrique Garcia <engapa@gmail.com>
2017-09-18 00:00:50 +0200

    Deploy/push docker image to dockerhub (latest from master branch)

--- zk_status.sh
+++ zk_status.sh
@@ -3,9 +3,9 @@
 ZK_clientPort=${ZK_clientPort:-2181}
 OK=$(echo ruok | nc 127.0.0.1 $ZK_clientPort)
 if [ "$OK" == "imok" ]; then
-  echo -e "\033[32mZookeeper server is running , :-)\033[0m"
+  echo -e "\033[32mZookeeper server is running, :-)\033[0m"
   exit 0
 else
-  echo -e "\033[31mZookeeper server is not running , :-(\033[0m"
+  echo -e "\033[31mZookeeper server is not running, :-(\033[0m"
   exit 1
 fi
