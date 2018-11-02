d121db4aae814f68bf94c706267310f4b51128e2
Enrique <engapa@gmail.com>
2018-11-02 11:47:24 +0100

    Openshift via minishift

--- openshift/main.sh
+++ openshift/main.sh
@@ -7,14 +7,9 @@ ZK_IMAGE="engapa/zookeeper:${ZK_VERSION}"
 
 MINISHIFT_VERSION=${MINISHIFT_VERSION:-"v1.26.1"}
 
-CHANGE_MINISHIFT_NONE_USER="true"
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-SLEEP_TIME=8
-MAX_ATTEMPTS=10
-
-function oc()
+function oc-install()
 {
   # Download oc
   curl -LO https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
@@ -22,9 +17,10 @@ function oc()
   mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc ./oc
   rm -rf openshift-origin-client-tools*
   chmod a+x oc
+  sudo mv oc /usr/local/bin/oc
 }
 
-function minishift()
+function minishift-install()
 {
   # Download minishift
   curl -LO https://github.com/minishift/minishift/releases/download/v1.26.1/minishift-1.26.1-linux-amd64.tgz
@@ -32,6 +28,7 @@ function minishift()
   mv minishift-1.26.1-linux-amd64/minishift ./minishift
   rm -rf minishift-*
   chmod a+x minishift
+  sudo mv minishift /usr/local/bin/minishift
 }
 
 function minishift-run()
@@ -43,11 +40,11 @@ function minishift-run()
   touch $HOME/.kube/config
 
   export KUBECONFIG=$HOME/.kube/config
-  sudo -E ./minishift start --memory 5000 --cpus 4
+  sudo -E ./minishift start --vm-driver=none --cpus 2
 
   # Waiting for Minikube
   for i in {1..150}; do # timeout for 5 minutes
-     ./oc version &> /dev/null
+     oc version &> /dev/null
      if [ $? -ne 1 ]; then
         break
     fi
@@ -61,8 +58,8 @@ function zk_install()
 {
 
   echo "Deploying zookeeper ..."
-  ./oc run zk --image $ZK_IMAGE --port 2181 --labels="component=zk,zk-name=zk"
-  ./oc expose deploy zk --name zk --port=2181 --cluster-ip=None --labels="component=zk,app=zk"
+  oc run zk --image $ZK_IMAGE --port 2181 --labels="component=zk,zk-name=zk"
+  oc expose deploy zk --name zk --port=2181 --cluster-ip=None --labels="component=zk,app=zk"
   echo "Zookeeper running on zk.<namespace>.svc.cluster.local"
 
 }
@@ -72,16 +69,17 @@ function zk_install()
 function check()
 {
 
-  attempts=0
-  JSONPATH_STSETS='replicasOk={.items[?(@.kind=="StatefulSet")].status.readyReplicas}'
-  until [ "$(./oc get -f $1 -o jsonpath="$JSONPATH_STSETS" 2>&1)" == "replicasOk=$2" ]; do
+  SLEEP_TIME=8
+  MAX_ATTEMPTS=10
+  ATTEMPTS=0
+  until [ "$(oc get -f $1 -o jsonpath='{.items[?(@.kind=="StatefulSet")].status.readyReplicas}' 2>&1)" == "replicasOk=$2" ]; do
     sleep $SLEEP_TIME
-    attempts=`expr $attempts + 1`
-    if [[ $attempts -gt $MAX_ATTEMPTS ]]; then
+    ATTEMPTS=`expr $ATTEMPTS + 1`
+    if [[ $ATTEMPTS -gt $MAX_ATTEMPTS ]]; then
       echo "ERROR: Max number of attempts was reached (${MAX_ATTEMPTS})"
       exit 1
     fi
-   echo "Retry [${attempts}] ... "
+   echo "Retry [${ATTEMPTS}] ... "
   done
 }
 
@@ -91,8 +89,8 @@ function test()
   file=$DIR/zk.yaml
   conf $file
   # When
-  ./oc create -f $file
-  ./oc new-app zk -p REPLICAS=1
+  oc create -f $file
+  oc new-app zk -p REPLICAS=1
   # Then
   check $file 3
   # TODO: Use zookeeper client to validate e2e
@@ -104,22 +102,22 @@ function test-persistent()
   install_zk
   file=$DIR/zk-persistent.yaml
   # When
-  ./oc create -f $file
-  ./oc new-app zk -p REPLICAS=1
+  oc create -f $file
+  oc new-app zk -p REPLICAS=1
   # Then
   check $file 3
 }
 
 function test-all()
 {
-  test && ./oc delete --force=true -l component=zk -l zk-name=zk all
-  test-persistent && ./oc delete --force=true -l component=zk -l zk-name=zk all,pv,pvc
+  test && oc delete --force=true -l component=zk -l zk-name=zk all
+  test-persistent && oc delete --force=true -l component=zk -l zk-name=zk all,pv,pvc
 }
 
 function clean() # Destroy minishift vm
 {
   echo "Cleaning ...."
-  ./minishift delete
+  minishift delete
 }
 
 function help() # Show a list of functions
