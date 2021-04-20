fe0d4b359970d3f666975e496b0c74f16523feeb
engapa <engapa@gmail.com>
2021-04-20 19:59:17 +0200

    using readyReplicas as status

--- k8s/main.sh
+++ k8s/main.sh
@@ -77,7 +77,7 @@ function check()
       echo "ERROR: Max number of attempts was reached (${MAX_ATTEMPTS})"
       exit 1
     fi
-   READY_REPLICAS=$(kubectl get -f $1 -o jsonpath='{.items[?(@.kind=="StatefulSet")].status.currentReplicas}' 2>&1)
+   READY_REPLICAS=$(kubectl get -f $1 -o jsonpath='{.items[?(@.kind=="StatefulSet")].status.readyReplicas}' 2>&1)
    echo "[${ATTEMPTS}/${MAX_ATTEMPTS}] - Ready zookeeper replicas : ${READY_REPLICAS:-0}/$2 ... "
   done
   kubectl get all
